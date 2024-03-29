ab79b2bf4ae608e77fb845ddf96abec6be1180e6
Francisco Cano <franffcano@gmail.com>
2024-03-29 20:22:51 -0300

    detalles finales, arregle el menu hamburguesa, e hice responsive las descripciones de los servicios

--- js/script.js
+++ js/script.js
@@ -1,28 +1,12 @@
-// Detectar el evento de desplazamiento y cambiar la clase de la barra de navegación
 window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
+    const scrollPosition = window.scrollY;
+    const menuContainer = document.querySelector('.menu-container');
+    
+    if (scrollPosition > 0) {
+      menuContainer.classList.add('scrolled');
     } else {
-        navbar.classList.remove('scrolled');
+      menuContainer.classList.remove('scrolled');
     }
-});
-
-// MenuHamburguesa
-
-const btnMenu = document.getElementsByClassName("btn-menu");
-const body = document.body
-for (let i = 0; i < btnMenu.length; i++) {
-    btnMenu[i].addEventListener('click', function () {
-        body.classList.toggle('menu-open');
-    });
-}
-
-function toggleNav() {
-    const navLinks = document.getElementsByClassName("nav-link");
-    for (let i = 0; i < navLinks.length; i++) {
-        navLinks[i].classList.toggle("menu-open");
-    }
-}
+  });
 
 
